cb717417297e2f1204424bf6472b4de40639d8c2
Frank Buters <fbuters@schubergphilis.com>
2020-10-14 17:54:17 +0200

    Incorporated feedback of Aatish

--- js/vue-definitions.js
+++ js/vue-definitions.js
@@ -259,7 +259,7 @@ let app = new Vue({
       { url: 'index-cs.html', language: 'Česky' },
       { url: 'index-uk.html', language: 'Українська' },
       { url: 'index-id.html', language: 'Bahasa Indonesia' },
-      { url: 'index-nl.html', language: 'Dutch' },
+      { url: 'index-nl.html', language: 'Nederlands' },
 
     ]
   },
