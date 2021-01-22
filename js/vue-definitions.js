0f0dc6dab437d56ce04f01cdc590bad7f512d06a
Darinka Zobenica <darinkazobenica@gmail.com>
2021-01-22 11:12:19 +0100

    added Serbian language to js translation list

--- js/vue-definitions.js
+++ js/vue-definitions.js
@@ -263,6 +263,7 @@ let app = new Vue({
       { url: 'index-nl.html', language: 'Nederlands' },
       { url: 'index-ca.html', language: 'Català' },
       { url: 'index-pl.html', language: 'Polski' },
+      { url: 'index-sr.html', language: 'Srpski' },
     ]
   },
 
